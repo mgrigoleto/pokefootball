61eea073f37a382a7fd3cf7ccef7fdb4060520b0
Marício Grigoleto <grigoleto.2002@gmail.com>
2026-07-17 10:58:26 -0300

    some touches on match simulation

--- src/pages/Match/index.js
+++ src/pages/Match/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './styles.css'
 import field from '../../assets/field.png'
+import { IoFootball } from "react-icons/io5"
 
 const Match = () => {
 
@@ -48,8 +49,12 @@ const Match = () => {
 
     const [score, setScore] = useState({ player: 0, enemy: 0 })
     const [activities, setActivities] = useState([])
+    const [time, setTime] = useState(0)
+    const [currentlySimulating, setCurrentlySimulating] = useState(false)
 
     const simulateMatch = () => {
+
+        setCurrentlySimulating(true)
 
         setScore({ player: 0, enemy: 0 })
 
@@ -154,8 +159,6 @@ const Match = () => {
         // Closer to zero means harder
         const difficultyMultipler = 0.8
 
-        let goals = { player: 0, enemy: 0 }
-
         for (let minute of eventMinutes) {
 
             // Get a random power value from the teams and apply the difficulty multiplier on it
@@ -181,8 +184,6 @@ const Match = () => {
                     }
 
                     auxActivities.push(actObj)
-
-                    goals = { ...goals, player: goals.player + 1 }
                 }
             } else {
                 const goal = attemptGoal(enemyPowerValue, playerPowerValue)
@@ -200,14 +201,26 @@ const Match = () => {
                     }
 
                     auxActivities.push(actObj)
-
-                    goals = { ...goals, enemy: goals.enemy + 1 }
                 }
             }
         }
 
-        setActivities(auxActivities)
-        setScore(goals)
+        for (let i = 0; i <= 90; i++) {
+            setTimeout(() => {
+                setTime(i)
+                const eventAtThisMinute = auxActivities.filter((act) => act.minute == i)
+                if (eventAtThisMinute?.[0]?.actor == 'player') {
+                    setScore((prev) => ({ ...prev, player: prev.player + 1 }))
+                } else if (eventAtThisMinute?.[0]?.actor == 'enemy') {
+                    setScore((prev) => ({ ...prev, enemy: prev.enemy + 1 }))
+                }
+                setActivities((curr) => [...curr, ...eventAtThisMinute])
+
+                if (i == 90) {
+                    setCurrentlySimulating(false)
+                }
+            }, i * 100)
+        }
     }
 
     return (
@@ -224,20 +237,33 @@ const Match = () => {
                 })}
             </div>
             <div className='field-activities'>
-                <label className='score'>
-                    <span className='player-name'>Bodde</span>
-                    <span className='live-score'>{score.player} - {score.enemy}</span>
-                    <span className='enemy-name'>AI Trainer</span>
-                </label>
+                <div className='match-header'>
+                    <label className='score'>
+                        <span className='player-name'>Bodde</span>
+                        <span className='live-score'>{score.player} - {score.enemy}</span>
+                        <span className='enemy-name'>AI Trainer</span>
+                    </label>
+                    <span className='timer'>{time}'</span>
+                </div>
                 <img src={field}></img>
                 <div className='match-activities'>
                     {activities?.map((event) => {
                         return (
-                            <label className={`${event.actor}-event `}>{event.minute}' {event.description}</label>
+                            <>
+                                <label className={`${event.actor}-event `}>
+                                    <IoFootball />
+                                    {event.minute}' {event.description}
+                                </label>
+                            </>
                         )
                     })}
                 </div>
-                <button onClick={() => simulateMatch()}>Start Game</button>
+                <button
+                    className='start-game-button'
+                    onClick={() => simulateMatch()}
+                    disabled={currentlySimulating}
+                >
+                    Start Game</button>
             </div>
             <div className='team-list'>
                 {enemyTeam?.map((player) => {
